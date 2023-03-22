91b9decaab770a1f2f199651d5d661972fb9ca5d
Thomas Gallaher <ThomasSGallaher@gmail.com>
2023-03-22 11:14:32 -0400

    Update DatabaseTesting.py

--- DatabaseTesting.py
+++ DatabaseTesting.py
@@ -1,15 +1,22 @@
 import DatabaseInteraction
 import sqlite3
 
-url = "google.com"
+def test():
+    url = "reddit.com"
+    print("testing")
+    connection = sqlite3.connect("PortalDB.db")
+    cursor = connection.cursor()
 
+    cursor.execute("INSERT INTO Forums (ForumURL) VALUES (?)", (url, ))
+    cursor.execute("SELECT * FROM Forums")
+    res = cursor.fetchall()
 
-def main():
-  connection = sqlite3.connect("PortalDB.db")
-  cursor = connection.cursor()
-  
-  forumList = ReadInDB(cursor):
-  AddForum(cursor, url, forumList)
-  UpdateSecurity(cursor, url, "Hello", "World", forumList)
-  BreakConnection(cursor, connection)
-  print(forumList[0])
+    for x in res:
+        print(x)
+
+    forumList = DatabaseInteraction.ReadInDB(cursor)
+    AddForum(cursor, url, forumList, connection)
+    UpdateSecurity(cursor, url, "Hello", "World", forumList, connection)
+    BreakConnection(cursor, connection)
+    print("tested")
+    print(forumList[4].URL)
