9c74acb5921a9632f81041cc62c7a8ca12c67a99
Thomas Gallaher <ThomasSGallaher@gmail.com>
2023-03-22 13:14:09 -0400

    Update DatabaseInteraction.py

--- DatabaseInteraction.py
+++ DatabaseInteraction.py
@@ -1,21 +1,36 @@
 import sqlite3
 import os
+import geticon
 
 #                       Database Structure                             #
+#                             Forums                                   #
 # ForumURL - Text Not Null                                   Holds URL #
 # Username - Text                                       Holds Username #
 # Password - Text                                       Holds password #
 # Icon - Text                             Holds file/filepath for file #
 
+#                            AllForums                                 #
+# URL - Text Not Null                                        Holds URL #
+
 
 class forum:
 
     def __init__(self, URL, Icon, Password, Username):
+        if (Password == ""):
+            noPass(self, URL, Icon)
+        else:
+            withPass(self, URL, Icon, Password, Username)
+    
+    def noPass(self, URL, Icon):
+        self.URL = URL
+        self.icon = Icon
+        
+    def withPass(self, URL, Icon, Password, Username):
         self.URL = URL
         self.icon = Icon
         self.Username = Username
         self.Password = Password
-
+        
     def setSecurity(self, Username, Password):
         self.Username = Username
         self.Password = Password
@@ -23,29 +38,26 @@ class forum:
 
 def ReadInDB(cursor):
     forumList = []
-    print("reading")
 
     cursor.execute("SELECT * FROM Forums")
-    #for row in cursor.execute("SELECT * FROM Forums"):
-    #    forumList.append(forum(row[0], row[1], row[2], row[3]))
     inList = cursor.fetchall()
     for row in inList:
-        #print("hello ", row)
-        forumList.append(forum(row[0], row[1], row[2], row[3]))
+        forumList.append(forum(row[0], row[1], "", ""))#row[2], row[3]))
     return forumList
 
 
 def AddForum(cursor, URL, list, connection):
-    #iconBin = convertImg(iconFile)
-    print("adding")
-    cursor.execute("INSERT INTO Forums (ForumURL) VALUES (?)", (URL, ))
+    cursor.execute("SELECT * FROM 'Forums' WHERE 'ForumsURL' = ?", URL)
+    if (cursor.fetchall().len() > 0):
+        return(2)
+    cursor.execute("INSERT INTO Forums (ForumURL) VALUES ?", URL)
     connection.commit()
-    list.append(forum(URL, "", "", ""))
+    geticon.download_favicon(URL)
+    list.append(forum(URL, os.path.basename(URL) + '.ico', "", ""))
     return list
 
 
 def UpdateSecurity(cursor, URL, Username, Password, list, connection):
-    print("updating")
     cursor.execute(
         "UPDATE Forums SET Username = ?, Password = ? WHERE ForumURL = ?",
         (URL, Username, Password))
@@ -58,8 +70,12 @@ def UpdateSecurity(cursor, URL, Username, Password, list, connection):
 
 
 def RemoveForum(cursor, URL, list, connection):
-    cursor.execute("DELETE FROM Forums WHERE ForumURL = ?", URL)
+    cursor.execute("SELECT 'Icon' FROM 'Forums' WHERE 'ForumsURL' = ?", URL)
+    file = cursor.fetchall()
+    os.remove(file)
+    cursor.execute("DELETE FROM 'Forums' WHERE 'ForumURL' = ?", URL)
     connection.commit()
+    
     for i in range(len(list)):
         if list[i].URL == URL:
             list.remove(i)
@@ -68,20 +84,6 @@ def RemoveForum(cursor, URL, list, connection):
 
 
 def BreakConnection(cursor, connection):
-    for item in imageFiles:
-        os.remove(item)
     if connection:
         cursor.close()
         del cursor
-
-
-def ConvertToBin(filename):
-    with open(filename, "rb") as file:
-        img = file.read()
-    return img
-
-
-def ConvertToImg(Bin, filename):
-    with open(filename, 'wb') as file:
-        file.write(Bin)
-
