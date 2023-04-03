de767bea179086484ec81a1af2bc3e65fabedf88
Thomas Gallaher <ThomasSGallaher@gmail.com>
2023-04-02 20:09:39 -0400

    Update DatabaseInteraction.py

--- DatabaseInteraction.py
+++ DatabaseInteraction.py
@@ -8,6 +8,7 @@ import geticon
 # Username - Text                                       Holds Username #
 # Password - Text                                       Holds password #
 # Icon - Text                             Holds file/filepath for file #
+# Name - Text                              Holds the name of the forum #
 
 #                            AllForums                                 #
 # URL - Text Not Null                                        Holds URL #
@@ -15,14 +16,39 @@ import geticon
 
 class forum:
 
-    def __init__(self, URL, Icon, Title):
+    def noPass(self, URL, Icon, Name):
         self.URL = URL
         self.icon = Icon
-        self.title = Title
-        
+        self.name = Name
+
+    def withPass(self, URL, Icon, Password, Username):
+        self.URL = URL
+        self.icon = Icon
+        self.Username = Username
+        self.Password = Password
+        self.name = Name
+
     def setSecurity(self, Username, Password):
         self.Username = Username
         self.Password = Password
+
+    def __init__(self, URL, Icon, Password, Username, Name):
+        self.URL = URL
+        self.icon = Icon
+        self.Username = Username
+        self.Password = Password
+        self.name = Name
+        #if (Password == ""):
+        #    noPass(self, URL, Icon)
+        #else:
+        #    withPass(self, URL, Icon, Password, Username)
+
+
+def FindForums(cursor):
+    allForumsFound = ScrapeForums()
+    sites = [(s, ) for s in links]
+    for item in sites:
+        cursor.execute("INSERT INTO AllForums (URL) VALUES (?)", item)
 
 
 def ReadInDB(cursor):
@@ -31,47 +57,44 @@ def ReadInDB(cursor):
     cursor.execute("SELECT * FROM Forums")
     inList = cursor.fetchall()
     for row in inList:
-        forumList.append(forum(row[0], row[1], "", ""))#row[2], row[3]))
+        forumList.append(forum(row[0], row[1], row[2], row[3], row[4]))
     return forumList
 
 
-def AddForum(cursor, URL, list, connection):
-    cursor.execute("SELECT * FROM 'Forums' WHERE 'ForumsURL' = ?", URL)
-    if (cursor.fetchall().len() > 0):
-        return(2)
-    cursor.execute("INSERT INTO Forums (ForumURL) VALUES ?", URL)
+def AddForum(cursor, URL, path, name, ls, connection):
+    cursor.execute(
+        "INSERT INTO Forums (ForumURL, Icon, Name) VALUES (?, ?, ?)",
+        (URL, path, name))
     connection.commit()
-    geticon.download_favicon(URL)
-    list.append(forum(URL, os.path.basename(URL) + '.ico', "", ""))
-    return list
+
+    ls.append(forum(URL, path, "", "", name))
+    return ls
 
 
-def UpdateSecurity(cursor, URL, Username, Password, list, connection):
+def UpdateSecurity(cursor, URL, Username, Password, ls, connection):
     cursor.execute(
         "UPDATE Forums SET Username = ?, Password = ? WHERE ForumURL = ?",
         (URL, Username, Password))
     connection.commit()
-    for x in list:
+    for x in ls:
         if x.URL == URL:
             x.setSecurity(Username, Password)
             break
-    return list
+    return ls
 
 
 def RemoveForum(cursor, URL, ls, connection):
-    cursor.execute("SELECT 'Icon' FROM 'Forums' WHERE 'ForumsURL' = ?", URL)
+    cursor.execute("SELECT 'Icon' FROM 'Forums' WHERE 'ForumsURL' = (?)",
+                   (URL, ))
     file = cursor.fetchall()
-    os.remove(file)
-    cursor.execute("DELETE FROM 'Forums' WHERE 'ForumURL' = ?", URL)
+    if len(file) > 0:
+        if file != "UNSET":
+            os.remove(file[0])
+    cursor.execute("DELETE FROM 'Forums' WHERE ForumURL = ?", (URL, ))
     connection.commit()
-    
-    ls.remove(i)
-    
-    for i in range(len(ls)):
-        if ls[i].URL == URL:
-            ls.remove(i)
-            break
-    return list
+    if URL in ls:
+        ls.remove(URL)
+    return ls
 
 
 def BreakConnection(cursor, connection):
