bcbcea486ab6a996b5364e643d3b77551ec7202e
k1dd <50468072+peter0x7f@users.noreply.github.com>
2023-04-03 19:03:59 -0700

    added remove button, background

--- portal.py
+++ portal.py
@@ -21,18 +21,64 @@ else:
     msg.setWindowTitle("Database Error")
     msg.setText("Unable to connect with the database.")
     x = msg.exec_()
+ 
+#set arrays global to be accessed from other classes without making removeItem child 
+global tab_name 
+global tab_link
+tab_name = []
+tab_link = []
 
+
+class removeItem(QMainWindow):
+    def __init__(self):
+        super(removeItem, self).__init__()
+ 
+        # setting title
+        self.setWindowTitle("Manage")
+        self.con = 0
+        # setting geometry
+        self.setGeometry(100, 100, 600, 400)
+        # calling method
+        self.UiComponents()
+ 
+    # method for widgets
+    def UiComponents(self):
+        # creating a combo box widget
+        self.combo_box = QComboBox(self)
+ 
+        # setting geometry of combo box
+        self.combo_box.setGeometry(200, 150, 120, 30)
+ 
+        # adding list of items to combo box
+        self.combo_box.addItems(tab_name)
+ 
+        # creating push button
+        self.submit = QPushButton("submit", self)
+ 
+        print(self.combo_box.count())
+ 
+        # adding action to button
+        self.submit.clicked.connect(self.pressed)
+        # setting geometry of the button
+        self.submit.setGeometry(200, 200, 200, 30)
+    def pressed(self):
+        if self.con == 0:
+            self.main = Window()
+            self.con =+ 1
+        self.content = self.combo_box.currentText()
+        self.main.removewidget(self.content)
+        self.close()
+        
+       
 
 class Window(QMainWindow):
-
     def __init__(self):
         super(Window, self).__init__()
-        self.forumList = DatabaseInteraction.ReadInDB(cursor)
+        self.con = 0
         self.initUI()
 
     def initUI(self):
-        self.tab_name = []
-        self.tab_link = []
+        self.setStyleSheet("background-color: #525252;")
         self.theapp = QApplication(['', '--no-sandbox'])
         self.mainurl = QWebEngineView()
         # set the title of main window
@@ -45,24 +91,29 @@ class Window(QMainWindow):
         self.height = int(0.618 * self.Width)
         self.resize(self.Width, self.height)
 
+
         self.add_forum = QPushButton('+', self)
         self.add_forum.move(0, int(0.618 * self.Width))
         self.add_forum.clicked.connect(self.popup)
+        self.rem_forum = QPushButton('-', self)
+        self.rem_forum.clicked.connect(self.delet)
+        self.rem_forum.clicked.connect(self.ui1)
 
         self.left_layout = QVBoxLayout()
         self.left_layout.addWidget(self.add_forum)
+        self.left_layout.addWidget(self.rem_forum)
         self.left_widget = QWidget()
         self.left_widget.setLayout(self.left_layout)
-
-        # for x in range(len(self.tab_)):
-        #     self.right_widget.addTab(QWidget(), self.tab_index[x][0])
+        #if array empty:
+        #populate array with db values
+        #   for x in range(len(self.tab_name)):
+        #     create_button(dbs entries)
 
         self.tab1 = self.ui1()
         self.right_widget = QTabWidget()
         self.right_widget.tabBar().setObjectName("mainTab")
 
         self.right_widget.addTab(self.tab1, '')
-
         self.right_widget.setCurrentIndex(0)
         self.right_widget.setStyleSheet('''QTabBar::tab{width: 0; \
             height: 0; margin: 0; padding: 0; border: none;}''')
@@ -75,63 +126,75 @@ class Window(QMainWindow):
         main_widget = QWidget()
         main_widget.setLayout(main_layout)
         self.setCentralWidget(main_widget)
-        self.create_button("reddit", "https://reddit.com")
 
     def ui1(self):
-        self.mainurl.setUrl(QtCore.QUrl(f'https://reddit.com'))
+        self.mainurl.setUrl(QtCore.QUrl(f'https://reddit.com/login'))
         return self.mainurl
-
-    def ui2(self, url):
-        self.mainurl.setUrl(QtCore.QUrl(f'{url}'))
-        return self.mainurl
-
+    
     def popup(self):
-        urldialog, ok = self.geturl.getText(self, "Add Forum", "Enter Url:")
+        urldialog, ok = self.geturl.getText(self, "Add Forum","Enter Url:")
         if ok:
             self.seturl.setText(urldialog)
             self.url_parse(self.seturl.text())
 
-    #dynamically sets button name and sets to params defined in ui2
-    def create_button(self, name, url):
-        path = GetIcon.download_favicon(url, name)
-        DatabaseInteraction.AddForum(cursor, url, path, name, self.forumList,
-                                     connection)
-        self.tab_name.append(name)
-        self.tab_link.append(url)
-        self.temp = len(self.tab_name)
-        globals()[f'{name}'] = QPushButton(name, self)
-        globals()[f'{name}'].setStyleSheet(
-            "border-radius : 25; border : 0px solid black")
-        globals()[f'{name}'].setIcon(QIcon(path))
-        globals()[f'{name}'].setIconSize(QSize(50, 50))
-        self.left_layout.addWidget(globals()[f'{name}'])
-        globals()[f'{name}'].clicked.connect(lambda: self.ui2(url))
+    def delet(self):
+        print(tab_name)
+        if len(tab_name) >= 1:
+            if self.con == 0:
+                self.thewindow=removeItem()
+                self.con=+1
+            self.thewindow.show()
+        else:
+            msg = QMessageBox()
+            msg.setWindowTitle(":/")
+            msg.setText("You have nothing to manage yet")
+            x = msg.exec_()
+    
 
+    def removewidget(self, name):
+        ind = tab_name.index(name)
+        tab_name.pop(ind)
+        tab_link.pop(ind)
+        name = globals()[f'{name}']
+        name.deleteLater()
+        
+
+    def create_button(self, name, url):
+        try:
+            download_favicon(url, name)
+        except:
+            print("..")
+        tab_name.append(name)
+        tab_link.append(url)
+        globals()[f'{name}'] = QPushButton(name, self)
+        self.left_layout.addWidget(globals()[f'{name}'])
+        globals()[f'{name}'].clicked.connect(lambda: self.mainurl.setUrl(QtCore.QUrl(f'{url}')))
+                
     @pyqtSlot()
     def url_parse(self, urlval):
 
         name = urlval
-        url = urlval
+        url = urlval 
 
         #checks if http to convert to https
         if "http://" in url:
-            url = url.replace("http://", "https://")
+            url = url.replace("http://","https://")
             name = url
 
         #checks if user entered https instead and if not adds it to their url
         elif "https://" not in url:
-            url = "https://" + url
+            url = "https://"+url
             name = url
 
-        #includes relavant headers
+        #includes relavant headers 
         headers = {
-            'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
         }
         #check if url is valid
         try:
             response = requests.get(url, headers=headers)
-            if response.status_code == 200:
+            print(response.status_code)
+            if response.status_code == 200 or response.status_code == 403:
                 #parses input for name
                 name = name.split('.')
                 if "https://www" in name[0]:
@@ -139,8 +202,15 @@ class Window(QMainWindow):
                 else:
                     name = name[0].split('://')
                     name = name[1][:]
-                self.create_button(name, url)
+                #call create button class
             response.close()
+            if url not in tab_link:
+                self.create_button(name, url)
+            else:
+                msg = QMessageBox()
+                msg.setWindowTitle("Invalid input")
+                msg.setText("You have already entered this URL")
+                x = msg.exec_()
         except:
             #alert user they entered invalid input
             msg = QMessageBox()
@@ -148,9 +218,12 @@ class Window(QMainWindow):
             msg.setText("The URL you entered is invalid, please try again.")
             x = msg.exec_()
 
+ 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Window()
     ex.show()
     sys.exit(app.exec_())
+
+
