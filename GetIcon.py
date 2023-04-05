e3f666be18146d90a5a801eaa7f6863da77e2f02
Thomas Gallaher <ThomasSGallaher@gmail.com>
2023-04-04 22:54:00 -0400

    Update GetIcon.py

--- GetIcon.py
+++ GetIcon.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 def download_favicon(url, name):
     #includes relavant headers 
-    headers = {a
+    headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
     #gets response
