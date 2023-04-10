d254e1f87fbb69f1f9494724cb5dba879fb1cc23
Thomas Gallaher <ThomasSGallaher@gmail.com>
2023-04-09 22:28:52 -0400

    Update portal.py

--- portal.py
+++ portal.py
@@ -1,17 +1,22 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5 import QtCore
-import sys
-import os
-import sqlite3
-import DatabaseInteraction
-import GetIcon
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+
+import sys
+import os
+import sqlite3
 import requests
 import atexit
 
+import DatabaseInteraction
+import GetIcon
+import ForumClass
+
+# TG start
+# connects the the database, validates connection, and creates a cursor
 connection = sqlite3.connect("PortalDB.db")
 if connection:
     cursor = connection.cursor()
@@ -20,7 +25,9 @@ else:
     msg.setWindowTitle("Database Error")
     msg.setText("Unable to connect with the database.")
     x = msg.exec_()
+# TG end
 
+# MM start
 #set arrays global to be accessed from other classes without making removeItem child
 global tab_name
 global tab_link
@@ -30,6 +37,7 @@ tab_link = []
 tab_icon = []
 
 
+# class for remove forum feature
 class removeItem(QMainWindow):
 
     def __init__(self):
@@ -55,14 +63,12 @@ class removeItem(QMainWindow):
         # creating push button
         self.submit = QPushButton("submit", self)
 
-        print(self.combo_box.count())
-
         # adding action to button
         self.submit.clicked.connect(self.pressed)
         # setting geometry of the button
         self.submit.setGeometry(200, 200, 200, 30)
-        print(self.con)
 
+    # method to connect button click to main functionality
     def pressed(self):
         if self.con == 0:
             self.main = Window()
@@ -71,11 +77,15 @@ class removeItem(QMainWindow):
         self.main.removewidget(self.content)
         self.combo_box.clear()
         self.hide()
-        
+
+
+"""
+# class to assist in forum search feature.
 class forumlist(QMainWindow):
+
     def __init__(self):
         super(forumlist, self).__init__()
- 
+
         # setting title
         self.setWindowTitle("Forum List")
         self.con = 0
@@ -83,36 +93,60 @@ class forumlist(QMainWindow):
         self.setGeometry(100, 100, 600, 400)
         # calling method
         self.UiComponents()
- 
+
     # method for widgets
     def UiComponents(self):
         # creating a combo box widget
         self.combo_box = QComboBox(self)
- 
+
         # setting geometry of combo box
         self.combo_box.setGeometry(200, 150, 120, 30)
- 
+
         # adding list of items to combo box
-        self.forum_list = ["https://www.reddit.com/","https://forums.craigslist.org/","https://www.quora.com/","http://stackoverflow.com/","https://gamefaqs.gamespot.com/","http://tianya.cn/","http://ign.com/boards","http://4chan.org/","https://www.ultimate-guitar.com/","https://www.xda-developers.com/","https://hackforums.net/","https://darkode.market/","http://slickdeals.net/","http://www.kaskus.co.id/","http://arstechnica.com/","https://bodybuilding.com","http://macrumors.com/","https://moneySavingExpert.com","http://www.teamliquid.net/","http://neogaf.com/","http://2ch.net/","https://ubuntuforums.org/","http://www.thestudentroom.co.uk/","http://sherdog.com/","https://LinuxQuestions.org","http://www.healthboards.com/","http://airliners.net/","http://www.pokecommunity.com/","http://www.pprune.org/","https://bitcointalk.org","https://www.blackhatworld.com/","https://www.phpbb.com/","https://disqus.com/"]
+        self.forum_list = [
+            "https://www.reddit.com/", "https://forums.craigslist.org/",
+            "https://www.quora.com/", "http://stackoverflow.com/",
+            "https://gamefaqs.gamespot.com/", "http://tianya.cn/",
+            "http://ign.com/boards", "http://4chan.org/",
+            "https://www.ultimate-guitar.com/",
+            "https://www.xda-developers.com/", "https://hackforums.net/",
+            "https://darkode.market/", "http://slickdeals.net/",
+            "http://www.kaskus.co.id/", "http://arstechnica.com/",
+            "https://bodybuilding.com", "http://macrumors.com/",
+            "https://moneySavingExpert.com", "http://www.teamliquid.net/",
+            "http://neogaf.com/", "http://2ch.net/",
+            "https://ubuntuforums.org/", "http://www.thestudentroom.co.uk/",
+            "http://sherdog.com/", "https://LinuxQuestions.org",
+            "http://www.healthboards.com/", "http://airliners.net/",
+            "http://www.pokecommunity.com/", "http://www.pprune.org/",
+            "https://bitcointalk.org", "https://www.blackhatworld.com/",
+            "https://www.phpbb.com/", "https://disqus.com/"
+        ]
+        if self.con == 0:
+            for item in self.forum_list:
+                if item in tab_link:
+                    self.forum_list.remove(item)
         self.combo_box.addItems(self.forum_list)
         # creating push button
         self.submit = QPushButton("add", self)
         self.combo_box.adjustSize()
-        print(self.combo_box.count())
- 
+
         # adding action to button
         self.submit.clicked.connect(self.pressed)
         # setting geometry of the button
         self.submit.setGeometry(200, 200, 200, 30)
-        print(self.con)
+
     def pressed(self):
         if self.con == 0:
             self.main = Window()
-            self.con =+ 1
+            self.con = +1
         self.content = self.combo_box.currentText()
         self.forum_list.remove(self.content)
-        self.main.url_parse(self.content)
-        self.hide()     
+        self.main.pass_args(self.content)
+        self.combo_box.clear()
+        self.hide()
+"""
+
 
 class Window(QMainWindow):
 
@@ -136,18 +170,27 @@ class Window(QMainWindow):
         self.height = int(0.618 * self.Width)
         self.resize(self.Width, self.height)
 
+        self.left_widget = QScrollArea()
+        self.left_widget.setWidgetResizable(True)
+
         self.left_layout = QVBoxLayout()
         self.left_layout.setAlignment(Qt.AlignTop)
         self.left_layout.setSpacing(30)
+        # MM end
+        # TG start
 
+        # horizontal layout to hold ui buttons
         self.uiBox = QHBoxLayout()
 
+        # add forum button
         self.addButton = QPushButton("", self)
         self.addButton.setIcon(QIcon("UI_Icons/AddButton.png"))
         self.addButton.setIconSize(QSize(30, 30))
         self.addButton.setStyleSheet(
             "border-radius : 15; border : 0px solid black")
         self.addButton.clicked.connect(self.popup)
+
+        # remove forum button
         self.minusButton = QPushButton("", self)
         self.minusButton.setIcon(QIcon("UI_Icons/MinusButton.png"))
         self.minusButton.setIconSize(QSize(35, 35))
@@ -156,25 +199,29 @@ class Window(QMainWindow):
         self.minusButton.clicked.connect(self.delet)
         self.minusButton.clicked.connect(self.ui1)
 
+        # the forum search feature is broken for some reason and we are debugging
+        """
         self.searchButton = QPushButton("", self)
         self.searchButton.setIcon(QIcon("UI_Icons/SearchButton.png"))
         self.searchButton.setIconSize(QSize(30, 30))
         self.searchButton.setStyleSheet(
             "border-radius : 15; border : 0px solid black")
         self.searchButton.clicked.connect(self.forums)
-        #self.left_layout.addWidget(b3)
+        """
+
+        # add buttons to the horizontal box widget
         self.uiBox.addWidget(self.addButton)
         self.uiBox.addWidget(self.minusButton)
-        self.uiBox.addWidget(self.searchButton)
+        #self.uiBox.addWidget(self.searchButton)
 
+        # make left_layout scrollable and add the uiBox to it.
         self.left_layout.addLayout(self.uiBox)
-        self.left_widget = QWidget()
-        self.left_widget.setLayout(self.left_layout)
-        #if array empty:
-        #populate array with db values
-        #   for x in range(len(self.tab_name)):
-        #     create_button(dbs entries)
+        self.inner = QFrame(self.left_widget)
+        self.inner.setLayout(self.left_layout)
+        self.left_widget.setWidget(self.inner)
+        # TG end
 
+        # MM start
         self.tab1 = self.ui1()
         self.right_widget = QTabWidget()
         self.right_widget.tabBar().setObjectName("mainTab")
@@ -183,6 +230,7 @@ class Window(QMainWindow):
         self.right_widget.setCurrentIndex(0)
         self.right_widget.setStyleSheet('''QTabBar::tab{width: 0; \
             height: 0; margin: 0; padding: 0; border: none;}''')
+
         #fix layout feature
         main_layout = QHBoxLayout()
         main_layout.addWidget(self.left_widget)
@@ -197,21 +245,21 @@ class Window(QMainWindow):
             for item in forumList:
                 self.create_button(item.name, item.URL, 1)
 
+    # sets the main page to the homepage html file
     def ui1(self):
-        #self.mainurl.setUrl(QtCore.QUrl(f'https://reddit.com/login'))
-        #self.mainurl.setUrl(QtCore.QUrl(f'Home.html'))
         self.mainurl.load(QtCore.QUrl.fromLocalFile(os.getcwd() +
                                                     '/Home.html'))
         return self.mainurl
 
+    # popup called by the add forum button
     def popup(self):
         urldialog, ok = self.geturl.getText(self, "Add Forum", "Enter Url:")
         if ok:
             self.seturl.setText(urldialog)
             self.url_parse(self.seturl.text())
 
+    # helper function to delete forums
     def delet(self):
-        print(tab_name)
         if len(tab_name) >= 1:
             if self.con == 0:
                 self.thewindow = removeItem()
@@ -224,31 +272,49 @@ class Window(QMainWindow):
             msg.setText("You have nothing to manage yet")
             x = msg.exec_()
 
+    # removes the widget and forum from lists when removing forums
     def removewidget(self, name):
+
         ind = tab_name.index(name)
 
+        # removes forum from buttons, database, and lists
         DatabaseInteraction.RemoveForum(cursor, tab_link[ind], connection)
         tab_name.pop(ind)
         tab_link.pop(ind)
         tab_icon.pop(ind)
         name = globals()[f'{name}']
         name.deleteLater()
-        
+
+    """ 
+    # part of the forum search feature
     def forums(self):
         if self.con1 == 0:
-            self.thewind=forumlist()
-            self.con1=+1
+            self.thewind = forumlist()
+            self.con1 = +1
+        self.thewind.combo_box.addItems(self.thewind.forum_list)
         self.thewind.show()
+    
 
+    def pass_args(self, url):
+        self.url_parse(url)
+    """
+
+    # MM end
+    # TG start
+    # dynamically creates buttons, if they are new adds them to the database, and downloads their icon.
     def create_button(self, name, url, action):
         path = GetIcon.download_favicon(url, name)
         if action == 0:
             DatabaseInteraction.AddForum(cursor, url, path, name, connection)
-
+# TG end
+# MM start
+# add the forum to the lists
         tab_name.append(name.lower())
         tab_link.append(url.lower())
         tab_icon.append(path)
         self.temp = len(tab_name)
+
+        # creates the button, sets size, icon, and connects a function
         globals()[f'{name}'] = QPushButton(name, self)
         globals()[f'{name}'].setStyleSheet(
             "border-radius : 25; border : 0px solid black")
@@ -258,6 +324,7 @@ class Window(QMainWindow):
         globals()[f'{name}'].clicked.connect(
             lambda: self.mainurl.setUrl(QtCore.QUrl(f'{url}')))
 
+    # parses the url and calls create_button.
     @pyqtSlot()
     def url_parse(self, urlval):
 
@@ -282,7 +349,6 @@ class Window(QMainWindow):
         #check if url is valid
         try:
             response = requests.get(url, headers=headers)
-            print(response.status_code)
             if response.status_code == 200 or response.status_code == 403:
                 #parses input for name
                 name = name.split('.')
@@ -308,7 +374,10 @@ class Window(QMainWindow):
             x = msg.exec_()
 
 
+# MM end
 
+# MM started this function, TG added to it.
+# looks messy atexit functions clean up application
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     atexit.register(app.deleteLater)
