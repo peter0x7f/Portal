565ca82159dbed2a23ada80882de1c0cc6f64b66
Thomas Gallaher <ThomasSGallaher@gmail.com>
2023-04-09 22:29:27 -0400

    Update GetIcon.py

--- GetIcon.py
+++ GetIcon.py
@@ -5,6 +5,8 @@ import os.path
 from bs4 import BeautifulSoup
 from PIL import Image
 
+# MM start
+
 
 def download_favicon(url, name):
     #includes relavant headers
@@ -12,24 +14,37 @@ def download_favicon(url, name):
         'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
+
     #gets response
     response = requests.get(url, headers=headers)
-    #double checks that
+
+    #double checks valid response
     if response.status_code == 200:
+
+        # MM end
+        # TG start
+        # checks if file already exists.
         if os.path.isfile(os.getcwd() + "/Icons/" + name + '.ico'):
             return "Icons/" + name + '.ico'
+        #TG end
+        # MM start
         soup = BeautifulSoup(response.content, 'html.parser')
+
         #checks for ico image
         icon_tag = soup.find('link', rel='shortcut icon')
+
+        #  checks alternate tag for icon
         if not icon_tag:
             icon_tag = soup.find('link', rel='icon')
         if icon_tag:
             icon_url = icon_tag['href']
             if 'http' not in icon_url:
                 icon_url = url + '/' + icon_url
+
             response = requests.get(icon_url, headers=headers, stream=True)
             #checks if ico link is valid
             if response.status_code == 200:
+                # generates full filepath for new icon file
                 icon_file = os.getcwd() + "/Icons/" + name + '.ico'
                 #writes content to file
                 with open(icon_file, 'wb') as f:
@@ -45,13 +60,16 @@ def download_favicon(url, name):
                 #sets icon to default
                 print('Failed to download favicon')
                 return "Icons/Default.png"
-                #return os.path.getcwd() + "Icons/Default.png"
+
         else:
             print('Favicon not found')
             return "Icons/Default.png"
-            #return os.path.getcwd() + "Icons/Default.png"
+
     else:
         print('Invalid website URL')
-        #return os.path.getcwd() + "Icons/Default.png"
         return "Icons/Default.png"
+
     response.close()
+
+
+# MM end
