3544a5fb06dfdc78d68995509146275ca9de7b50
Thomas Gallaher <ThomasSGallaher@gmail.com>
2023-04-09 22:29:46 -0400

    Update DatabaseInteraction.py

--- DatabaseInteraction.py
+++ DatabaseInteraction.py
@@ -1,47 +1,16 @@
 import sqlite3
 import os
 import GetIcon
+import ForumClass
 
 #                       Database Structure                             #
 #                             Forums                                   #
 # ForumURL - Text Not Null                                   Holds URL #
-# Username - Text                                       Holds Username #
-# Password - Text                                       Holds password #
 # Icon - Text                             Holds file/filepath for file #
 # Name - Text                              Holds the name of the forum #
-
+"""
 #                            AllForums                                 #
 # URL - Text Not Null                                        Holds URL #
-
-
-class forum:
-
-    def noPass(self, URL, Icon, Name):
-        self.URL = URL
-        self.icon = Icon
-        self.name = Name
-
-    def withPass(self, URL, Icon, Password, Username):
-        self.URL = URL
-        self.icon = Icon
-        self.Username = Username
-        self.Password = Password
-        self.name = Name
-
-    def setSecurity(self, Username, Password):
-        self.Username = Username
-        self.Password = Password
-
-    def __init__(self, URL, Icon, Password, Username, Name):
-        self.URL = URL
-        self.icon = Icon
-        self.Username = Username
-        self.Password = Password
-        self.name = Name
-        #if (Password == ""):
-        #    noPass(self, URL, Icon)
-        #else:
-        #    withPass(self, URL, Icon, Password, Username)
 
 
 def FindForums(cursor):
@@ -49,47 +18,47 @@ def FindForums(cursor):
     sites = [(s, ) for s in links]
     for item in sites:
         cursor.execute("INSERT INTO AllForums (URL) VALUES (?)", item)
+"""
+
+# TG start
 
 
+# returns a list of all forum objects containing all the forums in the database
 def ReadInDB(cursor):
     forumList = []
-
     cursor.execute("SELECT * FROM Forums")
     inList = cursor.fetchall()
     for row in inList:
-        forumList.append(forum(row[0], row[1], row[2], row[3], row[4]))
+        forumList.append(ForumClass.forum(row[0], row[1], row[2]))
     return forumList
 
 
+# adds a forum to the database
 def AddForum(cursor, URL, path, name, connection):
-    cursor.execute("SELECT * FROM 'Forums' WHERE 'ForumURL' = ?", (URL, ))
-    output = cursor.fetchall()
-    if len(output) == 0:
-        cursor.execute(
-            "INSERT INTO Forums (ForumURL, Icon, Name) VALUES (?, ?, ?)",
-            (URL, path, name))
-        connection.commit()
-
-
-def UpdateSecurity(cursor, URL, Username, Password, connection):
     cursor.execute(
-        "UPDATE Forums SET Username = ?, Password = ? WHERE ForumURL = ?",
-        (URL, Username, Password))
+        "INSERT INTO Forums (ForumURL, Icon, Name) VALUES (?, ?, ?)",
+        (URL, path, name))
     connection.commit()
+    return 0
 
 
+# removes a forum from the database and checks if an icon was downloaded. if so delete .ico file
 def RemoveForum(cursor, URL, connection):
-    cursor.execute("SELECT 'Icon' FROM 'Forums' WHERE 'ForumsURL' = (?)",
-                   (URL, ))
+    cursor.execute("SELECT Icon FROM 'Forums' WHERE ForumURL = (?)", (URL, ))
     file = cursor.fetchall()
-    if len(file) > 0:
-        if file != "UNSET":
-            os.remove(file[0])
+    file = file[0]
+    # checks if icon exists and is not default.
+    if len(file) > 0 and file[0] != "Icons/Default.png":
+        os.remove(file[0])
     cursor.execute("DELETE FROM 'Forums' WHERE ForumURL = ?", (URL, ))
     connection.commit()
 
 
+# breaks the connection with the database
 def BreakConnection(cursor, connection):
     if connection:
         cursor.close()
         del cursor
+
+
+# TG end
